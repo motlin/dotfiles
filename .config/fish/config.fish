270a860b3eca54c9ea3eb620740e5c3d019cc99a
Craig P. Motlin <cmotlin@gmail.com>
2020-05-30 11:25:03 -0400

    Add .config/fish, much of which is host specific.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,11 @@
 source ~/.alias
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/Craig/tools/google-cloud-sdk/path.fish.inc' ]; . '/Users/Craig/tools/google-cloud-sdk/path.fish.inc'; end
+set PATH $HOME/.jenv/bin $PATH
+status --is-interactive; and source (jenv init -|psub)
+
+set PATH /usr/local/opt/python@3.8/bin $PATH
+
+# Created by `userpath` on 2020-04-30 16:39:16
+set PATH $PATH /Users/Craig/.local/bin
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/Users/Craig/.local/share/omf/themes/bobthefish/fish_prompt.fish
